b5a57c04af0a1a4e7a79d0a09a9c7ff35cd6c4c3
Linh <l@blinkist.com>
2017-03-19 18:11:20 +0100

    Update cost function

--- ex2/costFunctionReg.m
+++ ex2/costFunctionReg.m
@@ -19,7 +19,7 @@ grad = zeros(size(theta));
 
 
 h = sigmoid(X*theta);
-J = (1/m) * (-y' * log(h) - (1 - y)' *  log(1 - h)) + (lambda/2*m) * theta^2;
+J = (1/m) * (-y' * log(h) - (1 - y)' *  log(1 - h)) + (lambda/2*m) * theta.^2;
 grad = (1/m) * X' * (h - y) + (lambda/m)*theta;
 
 
