67b539948a5e855b5d45c2f5e7f20d0a667c01e1
Linh <l@blinkist.com>
2017-03-22 23:41:34 +0100

    Update lrCostFunction

--- ex3/lrCostFunction.m
+++ ex3/lrCostFunction.m
@@ -36,13 +36,12 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
-
-
-
-
-
-
-
+h = sigmoid(X*theta);
+J = (1/m) * (-y' * log(h) - (1 - y)' *  log(1 - h));
+grad = (1/m) * X' * (h - y);
+temp = theta;
+temp(1) = 0; 
+grad = grad + (lambda/m) * theta1;
 
 
 % =============================================================
