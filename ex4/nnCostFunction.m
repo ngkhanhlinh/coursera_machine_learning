77da0981e82e9899e2dbff75b6b5f33c82084831
Linh <l@blinkist.com>
2017-03-30 22:10:28 +0200

    Added element-wise multiplication

--- ex4/nnCostFunction.m
+++ ex4/nnCostFunction.m
@@ -62,19 +62,18 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
+% Expand the 'y' output values into a matrix of single values
+y_matrix = eye(num_labels)(y,:);
 
-
-
-
-
-
-
-
-
-
-
-
-
+% Feedforward the neural network & return the cost in J
+a1 = [ones(m, 1) X]; % 5000 x 401
+z2 = a1 * Theta1'; % 5000 x 25 
+a2 = sigmoid(z2); % 5000 x 25
+a2 = [ones(size(a2, 1), 1) a2]; % 5000 x 26 
+z3 = a2 * Theta2'; % 5000 x 10 
+a3 = sigmoid(a2 * Theta2'); % 5000 x 10 
+h = a3; % 5000 x 10 
+J = (1 / m) * sum(sum(-y_matrix.* log(h) - (1 - y_matrix) .*  log(1 - h)));
 
 
 
