4e887c0a9b3ff838ab49f14a121994a23f36cd43
Linh <l@blinkist.com>
2017-04-19 22:22:24 +0200

    Processed and updated email features

--- ex6/processEmail.m
+++ ex6/processEmail.m
@@ -97,12 +97,9 @@ while ~isempty(email_contents)
     %       str2). It will return 1 only if the two strings are equivalent.
     %
 
-
-
-
-
-
-
+match = strcmp(str, vocabList);
+idx = find(match);
+word_indices = [word_indices ; idx]
 
 
 
